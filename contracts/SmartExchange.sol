d7d3753ba8c3d471c2a7c417a96e6b1e88cbb439
Opeyemi Olabode <opeyemi_o@bts.com.ng>
2021-04-10 20:19:01 +0100

    complete exchange cntract
    - tests still show revert errors
    - include system diagram

--- contracts/SmartExchange.sol
+++ contracts/SmartExchange.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.7.0;
 pragma experimental ABIEncoderV2;
 
+import "./Bep20.sol";
 
 //TODO: move to HelperPancakeSwap_ROUTER.sol
 contract HelperPancakeSwap_ROUTER {
@@ -9,8 +10,8 @@ contract HelperPancakeSwap_ROUTER {
     /*
     * @notice   Swaps an exact amount of input tokens for as many output tokens as possible, 
     *           along the route determined by the path. The first element of path is the input token, 
-    *           the last is the output token, and any intermediate elements represent intermediate pairs 
-    *           to trade through (if, for example, a direct pair does not exist). 
+    *           the last is the output token, and any intermediate elements represent intermediate pairs
+    *           to trade through (if, for example, a direct pair does not exist).
     *           ``msg.sender`` should have already given the router an allowance of at least amountIn on the input token.
     * @param    amountIn    The amount of input tokens to send.
     * @param    amountOutMin    The minimum amount of output tokens that must be received for the transaction not to revert.
@@ -51,16 +52,32 @@ contract HelperPancakeSwap_ROUTER {
 */
 contract SmartExchange {
     
-    uint256 public totalTreasury;
+    address public constant wBNB = 0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd; //underlying asset: BUSD
+    address public constant BUSD = 0x8301F2213c0eeD49a7E28Ae4c3e91722919B8B47; //underlying asset: BUSD
     address public constant pROUTER = 0xD99D1c33F9fC3444f8101754aBC46c52416550D1; // Pancakeswap Router (BSC Testnet)
-    address public constant pFACTORY = 0x6725F303b657a9451d8BA641348b6761A6CC7a17; // Pancakeswap factory (BSC Testnet)
+    //address public constant pFACTORY = 0x6725F303b657a9451d8BA641348b6761A6CC7a17; // Pancakeswap factory (BSC Testnet)
     
-    function invest(uint256 amount) public {
+   function swap(uint256 amount, address to) public returns (uint[] memory) {
         
-        HelperPancakeSwap_ROUTER underlying = HelperPancakeSwap_ROUTER(pFACTORY);     // get a handle for the exchange router
-        // VToken vToken = VToken(pROUTER);   // get a handle for the corresponding dex factory
-        // underlying.approve(address(vToken), amount); // approve the transfer
-        // assert(vToken.mint(amount) == 0);            // mint the vTokens and assert there is no error
-    }
-    
+        HelperPancakeSwap_ROUTER router = HelperPancakeSwap_ROUTER(pROUTER);     // get a handle for the exchange router
+        Bep20 busdContract = Bep20(BUSD);     // get a handle for the BUSD asset
+        Bep20 wbnbContract = Bep20(wBNB);     // get a handle for the wBNB asset
+        
+        wbnbContract.deposit{value: amount}(); // deposit native BNB
+        wbnbContract.approve(address(router), amount); // allow pancakeswap router to access wBNB
+
+        uint256 amountIn = amount;
+        uint256 amountOutMin = 0;
+        address[] memory path = new address[](2);
+        path[0] = address(wbnbContract);
+        path[1] = address(busdContract);
+        address to = to;
+        uint deadline = block.timestamp + 10 minutes; // addition 10 mins
+
+        uint[] memory amountOut = router.swapExactTokensForTokens(amountIn, amountOutMin, path, to, deadline);
+        
+        // assert(amountOut > 0, "swapping failed with returning 0");
+
+        return amountOut;
+    }      
 }
